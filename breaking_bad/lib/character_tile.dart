1b642e7f0bb4a9923340a234f3c054fb3322f226
breezybrow <browder.bree@gmail.com>
2022-05-06 00:20:27 +0000

    commit all changes to git

--- breaking_bad/lib/character_tile.dart
+++ breaking_bad/lib/character_tile.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'models.dart';
+import 'dart:convert';
+import 'dart:async';
 
 // Create a new stateless widget and give it a class attribute
-
 class CharacterTile extends StatelessWidget {
   final Character character;
   const CharacterTile({
@@ -16,25 +18,20 @@ class CharacterTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridTile(
         child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20,
-              childAspectRatio: 1),
-          itemBuilder: (BuildContext ctx, index) {
+              crossAxisSpacing: 15,
+              mainAxisSpacing: 15, ),
+          itemBuilder: (BuildContext context, int index) {
             return Container(
-              padding: const EdgeInsets.all(8),
+              padding: EdgeInsets.all(6),
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: NetworkImage(character.imgUrl),
                   fit: BoxFit.cover,
                 ),
               ),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  character.name,
-                  style: const TextStyle(color: Colors.white),
+              child: Align(alignment: Alignment.bottomLeft, child: Text(character.name, style: TextStyle(color: Colors.white),
                 ),
               ),
             );
